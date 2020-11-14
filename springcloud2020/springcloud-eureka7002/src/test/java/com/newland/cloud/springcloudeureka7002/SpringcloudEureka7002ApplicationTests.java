b30226abb8069867ce37481695e0dadb1007c770
lfp-123 <1406051995@qq.com>
2020-11-14 14:32:18 +0800

    zk

---
+++ springcloud2020/springcloud-eureka7002/src/test/java/com/newland/cloud/springcloudeureka7002/SpringcloudEureka7002ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newland.cloud.springcloudeureka7002;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudEureka7002ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
