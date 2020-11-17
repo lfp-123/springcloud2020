10b2d314968d1493c7dff35d89ae69add02a6784
lfp-123 <1406051995@qq.com>
2020-11-17 19:29:40 +0800

    111

---
+++ springcloud2020/springcloud-payment-getway/src/main/java/com/newalnd/cloud/springcloudpaymentgetway/SpringcloudPaymentGetwayApplication.java
@@ -0,0 +1,13 @@
+package com.newalnd.cloud.springcloudpaymentgetway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudPaymentGetwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudPaymentGetwayApplication.class, args);
+	}
+
+}
---
+++ springcloud2020/springcloud-payment-getway/src/test/java/com/newalnd/cloud/springcloudpaymentgetway/SpringcloudPaymentGetwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newalnd.cloud.springcloudpaymentgetway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudPaymentGetwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
