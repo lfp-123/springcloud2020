eb00e02c08555794664362fe281c655915c3d2c4
lfp-123 <1406051995@qq.com>
2020-11-18 19:44:02 +0800

    1

---
+++ springcloud2020/springcloud-alibaba-nacos/src/main/java/com/newland/cloud/springcloudalibabanacos/SpringcloudAlibabaNacosApplication.java
@@ -0,0 +1,13 @@
+package com.newland.cloud.springcloudalibabanacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudAlibabaNacosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudAlibabaNacosApplication.class, args);
+	}
+
+}
---
+++ springcloud2020/springcloud-alibaba-nacos/src/test/java/com/newland/cloud/springcloudalibabanacos/SpringcloudAlibabaNacosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newland.cloud.springcloudalibabanacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudAlibabaNacosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
