21e0a85e755d519b62668cebe1476191c53b263c
lfp-123 <1406051995@qq.com>
2020-11-18 19:45:03 +0800

    1

---
+++ springcloud2020/springcloudalibaba-payment9002/src/test/java/com/newland/cloud/springcloudalibabapayment9002/SpringcloudalibabaPayment9002ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newland.cloud.springcloudalibabapayment9002;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudalibabaPayment9002ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
